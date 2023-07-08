9e4b17aa43d76fde07654173b6ed71f1b1d442eb
CruncyChoco <45945815+unmeimusu@users.noreply.github.com>
2023-07-08 07:22:20 +0700

    Create bg.sh

---
+++ bg.sh
@@ -0,0 +1,11 @@
+cd ~/Download
+wget https://assets.unigine.com/d/Unigine_Valley-1.0.run
+chmod +x ~/Download/Unigine_Valley-1.0.run
+~/Download/Unigine_Valley-1.0.run
+cd ~/Download/Unigine_Valley-1.0
+
+# benchmark visualization with dedicated graphic card.
+DRI PRIME=1 ./valley.run
+
+# benchmark visualization with integrated graphic card.
+./valley.run
